05a22395cb7f132b29f76c2f31fce35d38412e2b
tasaquino <thaisandrade@live.com>
2023-07-05 22:39:10 +0200

    code improvement on places providers file

--- lib/features/places/provider/places_provider.dart
+++ lib/features/places/provider/places_provider.dart
@@ -2,24 +2,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:parking_app/features/places/data/places_repository_impl.dart';
-import 'package:parking_app/features/places/domain/places_interactor.dart';
 import 'package:parking_app/features/places/domain/places_interactor_impl.dart';
 import 'package:parking_app/features/places/presentation/notifiers/places_notifier.dart';
 import 'package:parking_app/features/places/presentation/notifiers/places_state.dart';
 
-final placesRepositoryProvider = Provider((_) {
-  return PlacesRepositoryImpl(http.Client());
-});
+final placesRepository = PlacesRepositoryImpl(http.Client());
 
-final placesInteractorProvider = Provider<PlacesInteractor>((ref) {
-  final repository = ref.watch(placesRepositoryProvider);
-  final interactor = PlacesInteractorImpl(repository);
-
-  return interactor;
-});
+final placesInteractorProvider = PlacesInteractorImpl(placesRepository);
 
 final placesNotifierProvider =
     StateNotifierProvider<PlacesNotifier, PlacesState>((ref) {
-  final interactor = ref.watch(placesInteractorProvider);
+  final interactor = placesInteractorProvider;
   return PlacesNotifier(interactor);
 });
